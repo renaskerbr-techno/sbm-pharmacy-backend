554e41d983b6a731387f16fd250c8d2acc2a1f51
RenaskeR Tech <renasker.br@gmail.com>
2026-02-26 14:27:36 +0530

    initial backend

--- firebase.js
+++ firebase.js
@@ -1,20 +1,15 @@
 const admin = require("firebase-admin");
-require("dotenv").config();
 
-const admin = require("firebase-admin");
-
-admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-  }),
-});
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    }),
+  });
+}
 
 const db = admin.firestore();
 
-module.exports = { db, admin };
+module.exports = { admin, db };
